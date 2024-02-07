a9591ef9f254a7fbbe118c49621715b22ad1bc57
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2024-02-07 15:53:27 -0600

    form created

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import "bulma/css/bulma.min.css"; // Import Bulma styles
+import "aos/dist/aos.css"; // Import AOS styles
+import AOS from "aos"; // Import AOS library
 
 const Contact = () => {
   const clienthub_id = "05e404a5-097b-465d-abf8-7f8496c3da02";
@@ -6,6 +9,9 @@ const Contact = () => {
     "https://clienthub.getjobber.com/client_hubs/05e404a5-097b-465d-abf8-7f8496c3da02/public/work_request/embedded_work_request_form";
 
   useEffect(() => {
+    // Initialize AOS
+    AOS.init();
+
     // Script for Jobber Work Request Embed
     const script = document.createElement("script");
     script.src =
@@ -21,19 +27,17 @@ const Contact = () => {
 
   return (
     <div>
-      <h2>Contact Lilie's Landscaping</h2>
+      <h2 className="title is-2" data-aos="fade-up">
+        Contact Lilie's Landscaping
+      </h2>
 
       {/* Button from external source */}
       <button
+        data-aos="zoom-in"
         name="button"
         type="button"
         id={`work-request-button-${clienthub_id}`}
-        style={{
-          display: "inline-flex",
-          color: "#ff9999",
-          backgroundColor: "#42A341",
-          // ... other styles
-        }}
+        className="button is-success has-text-danger-dark "
       >
         Request Your Free Quote
       </button>
