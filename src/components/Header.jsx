643d998ea17bcb40a7ee65842ed6b34cc2771f6b
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2024-02-07 21:34:50 -0600

    color scheme needs work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faInfo, faTools, faStar, faEnvelope, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../asstets/images/lilies landscaping.webp';
@@ -7,14 +7,13 @@ import './Header.css'; // Import a separate CSS file for styling
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Add the AOS styles
 
-
 const Header = () => {
   useEffect(() => {
     AOS.init({
       duration: 2000, // Set the default duration (in milliseconds)
     });
   }, []);
-  
+
   return (
     <header>
       <div className="logo-container" data-aos="fade-right">
@@ -24,17 +23,40 @@ const Header = () => {
       </div>
       <nav>
         <ul>
-          <li><Link to="/"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
-          <li><Link to="/about"><FontAwesomeIcon icon={faInfo} /> About</Link></li>
-          <li><Link to="/services"><FontAwesomeIcon icon={faTools} /> Services</Link></li>
-          <li><Link to="/testimonials"><FontAwesomeIcon icon={faStar} /> Testimonials</Link></li>
-          <li><Link to="/contact"><FontAwesomeIcon icon={faEnvelope} /> Contact</Link></li>
-          <li className="login-link"><Link to="/login"><FontAwesomeIcon icon={faSignInAlt} /> Login</Link></li>
+          <li>
+            <NavLink exact to="/" activeClassName="active">
+              <FontAwesomeIcon icon={faHome} /> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="active">
+              <FontAwesomeIcon icon={faInfo} /> About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" activeClassName="active">
+              <FontAwesomeIcon icon={faTools} /> Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/testimonials" activeClassName="active">
+              <FontAwesomeIcon icon={faStar} /> Testimonials
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeClassName="active">
+              <FontAwesomeIcon icon={faEnvelope} /> Contact
+            </NavLink>
+          </li>
+          <li className="login-link">
+            <Link to="/login">
+              <FontAwesomeIcon icon={faSignInAlt} /> Login
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
   );
 };
-
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,17 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './Home.css';
 
-
 const Home = () => {
   useEffect(() => {
     AOS.init();
   }, []);
 
   return (
-    <div className="has-text-centered">
+    <div className="home-container">
       <section className="hero is-primary" data-aos="fade-in">
         <div className="hero-body">
           <div className="container">
-            <h1 className="title is-size-2 " data-aos="fade-right">LILIE'S LANDSCAPE LLC</h1>
+            <h1 className="title is-size-2" data-aos="fade-right">LILIE'S LANDSCAPE LLC</h1>
           </div>
         </div>
       </section>
@@ -25,14 +24,10 @@ const Home = () => {
       <section className="section" data-aos="fade-up">
         <div className="container">
           <div className="columns is-centered">
-            <div className="column is-6">
-              <h3 className="title is-size-3" >
-                Your property is our business.
-              </h3>
+            <div className="column is-8">
+              <h3 className="title is-size-3">Your property is our business.</h3>
               <p>
-                Lilies offers comprehensive commercial and residential landscape
-                services. From planning and design to installation, management,
-                and irrigation.
+                Lilies offers comprehensive commercial and residential landscape services. From planning and design to installation, management, and irrigation.
               </p>
             </div>
           </div>
@@ -43,58 +38,38 @@ const Home = () => {
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-4" data-aos="zoom-in">
-              <div className="box has-background-primary">
-                <h3 className="title is-size-3 has-text-white">
-                  EXPERIENCED AND CREATIVE LANDSCAPERS
-                </h3>
-                <p className="has-text-white">
-                  We specialize in helping clients increase the value and appeal
-                  of their property by creating their dream outdoor space. We
-                  create beautiful landscape designs by combining seasonals,
-                  perennials, contemporary landscaping, and regular maintenance.
+              <div className="box">
+                <h3 className="title is-size-3">EXPERIENCED AND CREATIVE LANDSCAPERS</h3>
+                <p>
+                  We specialize in helping clients increase the value and appeal of their property by creating their dream outdoor space. We create beautiful landscape designs by combining seasonals, perennials, contemporary landscaping, and regular maintenance.
                 </p>
-                <p className="has-text-white">
-                  Our goal is to ensure that you are completely satisfied with the
-                  appeal of your property.
+                <p>
+                  Our goal is to ensure that you are completely satisfied with the appeal of your property.
                 </p>
               </div>
             </div>
             <div className="column is-4" data-aos="zoom-in">
-              <div className="box has-background-link">
-                <h3 className="title is-size-3 has-text-white">
-                  WE CARE AND IT SHOWS
-                </h3>
-                <p className="has-text-white">
-                  We pay attention to the smallest details with each customer to
-                  ensure complete satisfaction. Our goal is to enhance your
-                  community, raise property values, and make your property
-                  beautiful.
+              <div className="box">
+                <h3 className="title is-size-3">WE CARE AND IT SHOWS</h3>
+                <p>
+                  We pay attention to the smallest details with each customer to ensure complete satisfaction. Our goal is to enhance your community, raise property values, and make your property beautiful.
                 </p>
-                <p className="has-text-white">
-                  We provide unmatched quality and service from the thought
-                  process to design all the way to the installation and
-                  maintenance of your property.
+                <p>
+                  We provide unmatched quality and service from the thought process to design all the way to the installation and maintenance of your property.
                 </p>
               </div>
             </div>
             <div className="column is-4" data-aos="zoom-in">
-              <div className="box has-background-success">
-                <h3 className="title is-size-3 has-text-white">
-                  SATISFACTION GUARANTEED
-                </h3>
-                <p className="has-text-white">
-                  Your satisfaction is our priority, and we strive to provide a
-                  service we are proud of.
+              <div className="box">
+                <h3 className="title is-size-4">SATISFACTION GUARANTEED</h3>
+                <p>
+                  Your satisfaction is our priority, and we strive to provide a service we are proud of.
                 </p>
-                <p className="has-text-white">
-                  We start every project off with a consultation to better
-                  understand your goals and needs.
+                <p>
+                  We start every project off with a consultation to better understand your goals and needs.
                 </p>
-                <p className="has-text-white">
-                  Each project is completed according to your preferences.
-                </p>
-                <p className="has-text-white">
-                  Let Lilies Landscaping earn your trust.
+                <p>
+                  Each project is completed according to your preferences. Let Lilies Landscaping earn your trust.
                 </p>
               </div>
             </div>
@@ -106,7 +81,6 @@ const Home = () => {
         <div className="container">
           <h3 className="title">SERVICES</h3>
           <div className="columns is-centered">
-            {/* First Row */}
             <div className="column is-4">
               <ul>
                 <li className="is-size-4">Residential and HOA Maintenance</li>
@@ -114,56 +88,56 @@ const Home = () => {
             </div>
             <div className="column is-4">
               <ul>
-                <li className="is-size-3">Tree Trimming and Removal</li>
+                <li className="is-size-4">Tree Trimming and Removal</li>
               </ul>
             </div>
             <div className="column is-4">
               <ul>
-                <li className="is-size-3">Commercial Landscape</li>
+                <li className="is-size-4">Commercial Landscape</li>
               </ul>
             </div>
           </div>
           <div className="columns is-centered">
-            {/* Second Row */}
             <div className="column is-6">
               <ul>
-                <li className="is-size-3">Litter and Debris Removal</li>
+                <li className="is-size-4">Litter and Debris Removal</li>
               </ul>
             </div>
             <div className="column is-6">
               <ul>
-                <li className="is-size-3">Landscape Design and Installation</li>
+                <li className="is-size-4">Landscape Design and Installation</li>
               </ul>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="section has-background-link-info" data-aos="fade-up">
+      <section className="section has-background-info" data-aos="fade-up">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
-              <div className="box">
-                {/* Set the color using the style attribute */}
-                <p className="title is-size-1" style={{ color: '#000' }}>
+            <div className="column">
+              <div className="box has-text-centered">
+                <p className="title is-size-1">
                   <FontAwesomeIcon icon={faShieldAlt} />
                 </p>
                 <p className="title is-size-3">Fully Insured</p>
               </div>
             </div>
-            <div className="column is-one-third box">
-              {/* Set the color using the style attribute */}
-              <p className="title is-size-1" style={{ color: '#000' }}>
-                <FontAwesomeIcon icon={faCheckCircle} />
-              </p>
-              <p className="title is-size-3">Reliable & Trustworthy</p>
+            <div className="column">
+              <div className="box has-text-centered">
+                <p className="title is-size-1">
+                  <FontAwesomeIcon icon={faCheckCircle} />
+                </p>
+                <p className="title is-size-3">Reliable & Trustworthy</p>
+              </div>
             </div>
-            <div className="column is-one-third box">
-              {/* Set the color using the style attribute */}
-              <p className="title is-size-1" style={{ color: '#000' }}>
-                <FontAwesomeIcon icon={faClipboardCheck} />
-              </p>
-              <p className="title is-size-3">Professional & Experienced</p>
+            <div className="column">
+              <div className="box has-text-centered">
+                <p className="title is-size-1">
+                  <FontAwesomeIcon icon={faClipboardCheck} />
+                </p>
+                <p className="title is-size-3">Professional & Experienced</p>
+              </div>
             </div>
           </div>
         </div>
